b7c81db6dc0c8a8eb759a5b7e39156f11ea58815
ajit <ajitteli@gmail.com>
2013-08-25 02:00:46 +0530

    demo_app second checkin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 DemoApp::Application.routes.draw do
+  resources :microposts
+  resources :users
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -11,7 +15,7 @@ DemoApp::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+     resources :users
 
   # Sample resource route with options:
   #   resources :products do
@@ -48,11 +52,12 @@ DemoApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'users#index'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  match ':controller(/:action)'
+  match ':controller(/:action(/:id))(.:format)'
 end
