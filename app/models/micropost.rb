2259c73889f01736ef6b51b823c89e653b1d59f3
ajit <ajitteli@gmail.com>
2013-08-25 02:23:45 +0530

    Modified some code.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,5 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
   belongs_to :user
-  validates :content, :length => {:maximum => 10, :too_long => ''}
+  validates :content, :length => { :maximum => 140 }
 end
