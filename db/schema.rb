6cc1ebcff1048c70fe7a90841714eaa280fc9c98
ajit <ajit@ajit.(none)>
2014-04-02 17:10:50 +0530

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140324131737) do
+ActiveRecord::Schema.define(:version => 20140402070243) do
 
   create_table "microposts", :force => true do |t|
-    t.string   "content"
+    t.text     "content"
     t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.integer  "to_user"
+    t.integer  "to_user_id"
   end
 
   create_table "users", :force => true do |t|
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(:version => 20140324131737) do
     t.datetime "locked_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
