788f2a4d6d6b323d0bcc16066bf87f6c9d24f7c1
Ajit <ajitteli@gmail.com>
2014-04-04 14:10:37 +0530

    mailer enable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ DemoApp::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -33,7 +33,9 @@ DemoApp::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
-
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.assets.debug = false
+  ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => "utf-8"
 end
